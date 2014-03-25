1a12d273cb18499eceba3ca6caba4d5686cff6ac
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-03-25 15:47:07 +0100

    Remove boost dependency - it's pointless

--- common.mk
+++ common.mk
@@ -5,5 +5,5 @@ AM_CXXFLAGS = -I$(top_srcdir) @AM_CXXFLAGS@
 AM_CXXFLAGS += -g --std=c++0x
 
 # Dependencies XXX keep these in sync with twine.pc.in
-AM_CXXFLAGS += $(BOOST_CPPFLAGS)
+AM_CXXFLAGS += $(PTHREAD_CFLAGS)
 # AM_LDFLAGS += 
