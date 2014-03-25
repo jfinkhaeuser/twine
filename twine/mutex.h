383717fb89f117cfd238706c10e4bc4928f38ab5
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-03-25 17:10:55 +0100

    Fix documentation

--- twine/mutex.h
+++ twine/mutex.h
@@ -33,34 +33,12 @@ namespace twine {
  * Mutex class
  *
  * This is a mutual exclusion object for synchronizing access to shared
- * memory areas for several threads.
+ * memory areas for several threads. It's an abstraction over system
+ * provided mutexes.
  *
- * On supported platforms/configurations, it is implemented as an atomic
- * spin lock with very low CPU overhead; elsewhere it uses system level
- * functions.
- *
- * FIXME
+ * There's a variant for recursive and a variant for non-recursive mutexes
+ * called recursive_mutex and mutex respectively.
  **/
-/// Fast mutex class.
-/// This is a mutual exclusion object for synchronizing access to shared
-/// memory areas for several threads. It is similar to the tthread::mutex class,
-/// but instead of using system level functions, it is implemented as an atomic
-/// spin lock with very low CPU overhead.
-///
-/// The \c fast_mutex class is NOT compatible with the \c condition_variable
-/// class (however, it IS compatible with the \c lock_guard class). It should
-/// also be noted that the \c fast_mutex class typically does not provide
-/// as accurate thread scheduling as a the standard \c mutex class does.
-///
-/// Because of the limitations of the class, it should only be used in
-/// situations where the mutex needs to be locked/unlocked very frequently.
-///
-/// @note The "fast" version of this class relies on inline assembler language,
-/// which is currently only supported for 32/64-bit Intel x86/AMD64 and
-/// PowerPC architectures on a limited number of compilers (GNU g++ and MS
-/// Visual C++).
-/// For other architectures/compilers, system functions are used instead.
-
 template <
   typename recursion_policyT
 >
