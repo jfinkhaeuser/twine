a4890745ff76944bd4c2f9c5ecc13d8dfc9b546d
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-03-31 10:05:11 +0200

    Fixes for making "make distcheck" work.

---
+++ twine/win32/thread.cpp
@@ -0,0 +1,28 @@
+/**
+ * This file is part of twine.
+ *
+ * Author(s): Jens Finkhaeuser <jens@unwesen.co.uk>
+ *
+ * Copyright (c) 2014 Unwesen Ltd.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#include <twine/thread.h>
+
+namespace twine {
+
+
+
+
+
+} // namespace twine
