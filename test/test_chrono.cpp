c7395534cc41ec33953aab929f4507770863c94e
Jens Finkhaeuser <jens@finkhaeuser.de>
2014-12-08 12:03:26 +0100

    Improve test's resilience against timing issues - it's still flakey, but it makes more sense now.

--- test/test_chrono.cpp
+++ test/test_chrono.cpp
@@ -249,9 +249,9 @@ private:
       tc::sleep(tc::milliseconds(25).convert<tc::nanoseconds>());
       tc::nanoseconds after = tc::now();
 
-      tc::nanoseconds diff = after - now;
-      CPPUNIT_ASSERT_MESSAGE("may fail if the system time changed or there is high CPU load",
-          std::labs(diff.raw() - 25000000) < 100000);
+      tc::default_repr_t diff = std::labs((after - now).raw() - 25000000);
+      CPPUNIT_ASSERT_MESSAGE("may fail if the system time changed", diff > 0);
+      CPPUNIT_ASSERT_MESSAGE("may fail if there is high CPU load", diff < 200000);
     }
 
 
